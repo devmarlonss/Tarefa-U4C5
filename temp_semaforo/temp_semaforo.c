3cd6afcf24ddd9e17b2d8d5f6fdb4151bcba4cf2
Marlon Santos <82154609+marloon-mon@users.noreply.github.com>
2025-02-01 18:07:18 -0300

    feat: função de inicialização dos pinos dos leds

--- temp_semaforo/temp_semaforo.c
+++ temp_semaforo/temp_semaforo.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/timer.h"
 
 // Definições dos pinos dos leds
 #define LED_PIN_R 13
 #define LED_PIN_G 11
 #define LED_PIN_B 12
 
+// Função de inicialização e configuração das portas gpio dos leds
+void led_init() {
+    gpio_init(LED_PIN_R);
+    gpio_set_dir(LED_PIN_R, GPIO_OUT);
+    gpio_init(LED_PIN_G);
+    gpio_set_dir(LED_PIN_G, GPIO_OUT);
+    gpio_init(LED_PIN_B);
+    gpio_set_dir(LED_PIN_B, GPIO_OUT);
+}
+
 int main() {
     stdio_init_all();
+
+    led_init();
 
     while (true) {
     }
