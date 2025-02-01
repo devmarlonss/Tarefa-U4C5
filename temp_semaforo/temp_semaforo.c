6c1b0a1fa6fea87cd0a66b9519d00dd7f0d7d008
Marlon Santos <82154609+marloon-mon@users.noreply.github.com>
2025-02-01 18:04:59 -0300

    feat: definições dos pinos dos leds

--- temp_semaforo/temp_semaforo.c
+++ temp_semaforo/temp_semaforo.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+// Definições dos pinos dos leds
+#define LED_PIN_R 13
+#define LED_PIN_G 11
+#define LED_PIN_B 12
 
 int main() {
     stdio_init_all();
 
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
     }
 }
